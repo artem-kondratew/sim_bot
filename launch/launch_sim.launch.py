74f37afe4a7904f13e5cf5f075e77463fab37db6
Roman Eidelman <reiv.dev@gmail.com>
2023-08-25 17:11:03 +0300

    Upgraded launch file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -20,6 +20,13 @@ def generate_launch_description():
                     get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
+    
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
@@ -50,6 +57,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
         gazebo,
         spawn_entity,
         diff_drive_spawner,  # Uncommit for ros2_control
